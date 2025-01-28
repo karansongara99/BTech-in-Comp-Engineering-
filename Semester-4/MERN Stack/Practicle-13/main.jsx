7badfff78f00737a1813db6ca985867da7bf4c7c
Karan Songara <karansongara290@gmail.com>
2025-01-28 20:09:03 +0530

    Setting up ReactJS development Environment

---
+++ Semester-4/MERN Stack/Practicle-13/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import Abc from './Practical-14/FunctionComponent'
+import ClassComponent from './Practical-14/classComponent'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <h1>Hello Word!</h1>
+  </>,
+)
