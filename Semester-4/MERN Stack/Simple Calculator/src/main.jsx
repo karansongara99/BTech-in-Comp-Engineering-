5ec11c71122328dce0fadb187971d69da1f30b2b
Karan Songara <karansongara290@gmail.com>
2025-02-22 19:29:13 +0530

    Simple Calculator In React Vite

---
+++ Semester-4/MERN Stack/Simple Calculator/src/main.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
